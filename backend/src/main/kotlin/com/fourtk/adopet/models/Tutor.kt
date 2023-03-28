4b28d73d1c0f014a459866901e9db25feb20b9b0
Dev-JeanSantos <jeancbsan@gmail.com>
2023-03-28 17:03:15 -0300

    create web project creation

--- backend/src/main/kotlin/com/fourtk/adopet/models/Tutor.kt
+++ backend/src/main/kotlin/com/fourtk/adopet/models/Tutor.kt
@@ -1,6 +1,7 @@
 package com.fourtk.adopet.models
 
 import jakarta.persistence.*
+import jakarta.validation.constraints.Email
 
 @Entity
 @Table(name = "db_tutor")
@@ -9,8 +10,10 @@ data class Tutor (
         @GeneratedValue(strategy = GenerationType.IDENTITY)
         val id: Long? = null,
         var name: String,
+        @Column(nullable = false, unique = true)
         var email: String,
         var password: String,
+        @Column(nullable = false, unique = true)
         var cpf: String,
         var address: String,
         var city: String,
